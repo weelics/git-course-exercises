fee04a2e12257f1a7cdf6a93df450068e19ad58c
weelics <weelixhill@gmail.com>
2022-10-19 18:31:35 +0200

    feat: random function

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,19 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+
+int getRandom(){
+  srand(time(NULL));
+  return rand();
+}
+
+int main(){
+  cout<< getRandom()<<endl;
+  return 0;
+}
